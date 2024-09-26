b606ec537a235f49691330e6284ad94252fad5aa
frankmwangi <frankmwangi8118@gmail.com>
2024-09-26 16:29:44 +0300

    corrected some configs

--- src/main/java/com/mwas/securityjwt106/Clients/UserServices.java
+++ src/main/java/com/mwas/securityjwt106/Clients/UserServices.java
@@ -24,6 +24,7 @@ public class UserServices {
 
     public Web_Users registerUser(Web_Users webUsers) {
         webUsers.setPassword(PasswordEncoder.encode(webUsers.getPassword()));
+        webUsers.setRoles("USER");
         return userRepository.save(webUsers);
     }
 
